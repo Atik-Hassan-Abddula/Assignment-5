52189d0fddce0efa980143ce8e9388e0bb494ee5
Atik-Hasan-55 <atik508030@gmail.com>
2026-09-13 11:15:55 +0600

    Navbar

--- src/Componets/Navbar.tsx
+++ src/Componets/Navbar.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
+// import React from 'react';
+import logo from '../assets/logo-text.png'
 
 const Navbar = () => {
     return (
-        <nav>
+        <nav className='flex justify-between container mx-auto px-8 pt-3.5'>
             <div>
-                <img src="" alt="" />
+                <img src={logo} alt="" />
             </div>
-            <ul>
-                <li>Home</li>
-                <li>Technologies</li>
-                <li>Projects</li>
-                <li>Home</li>
-                <li>Contact</li>
+            <ul className='flex gap-4 '>
+                <li className='hover:text-[#DB2777]'>Home</li>
+                <li className='hover:text-[#DB2777]'>Technologies</li>
+                <li className='hover:text-[#DB2777]'>Projects</li>
+                <li className='hover:text-[#DB2777]'>Home</li>
+                <li className='hover:text-[#DB2777]'>Contact</li>
             </ul>
             <div>
-                <button>Sign In</button>
-            </div><button>Sign Up</button>
+                <button >Sign In</button>
+                <button className='py-1.5 px-4 m-2 bg-[#DB2777] rounded-3xl'>Sign Up</button>
 
+            </div>
 
 
 
