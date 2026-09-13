bef99de61ee43a2c40a76a1b53dd11b2717d0dd5
Atik-Hasan-55 <atik508030@gmail.com>
2026-09-13 10:48:09 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from "./Componets/Navbar"
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <Navbar></Navbar>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Componets/Navbar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Technologies</li>
+                <li>Projects</li>
+                <li>Home</li>
+                <li>Contact</li>
+            </ul>
+            <div>
+                <button>Sign In</button>
+            </div><button>Sign Up</button>
+
+
+
+
+        </nav>
+    );
+};
+
+export default Navbar;
